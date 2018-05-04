5f949868b7768e80a8a1c74a0a2fea779844342b
Guilherme Felipe Wetzstein <guilherme@conectra.net.br>
2018-05-04 10:17:00 -0300

    Indent.

--- cap2-http/desafio/app.js
+++ cap2-http/desafio/app.js
@@ -3,29 +3,29 @@ const url = require('url');
 const fs = require('fs');
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/html'});
+    response.writeHead(200, {'Content-Type': 'text/html'});
 
-  const result = url.parse(request.url, true);
-  switch(result.pathname) {
-    case "/":
-    case "/artigos":
-      fs.readFile(`${__dirname}/artigos.html`, (erro, html) => {
-        response.write(html);
-        response.end();
-      });
-      break;
-    case "/contato":
-      fs.readFile(`${__dirname}/contato.html`, (erro, html) => {
-        response.write(html);
-        response.end();
-      });
-      break;
-    default:
-      fs.readFile(`${__dirname}/erro.html`, (erro, html) => {
-        response.write(html);
-        response.end();
-      });
-  }
+    const result = url.parse(request.url, true);
+    switch (result.pathname) {
+        case "/":
+        case "/artigos":
+            fs.readFile(`${__dirname}/artigos.html`, (erro, html) => {
+                response.write(html);
+                response.end();
+            });
+            break;
+        case "/contato":
+            fs.readFile(`${__dirname}/contato.html`, (erro, html) => {
+                response.write(html);
+                response.end();
+            });
+            break;
+        default:
+            fs.readFile(`${__dirname}/erro.html`, (erro, html) => {
+                response.write(html);
+                response.end();
+            });
+    }
 });
 
 server.listen(3000, () => {
--- cap2-http/hello_server.js
+++ cap2-http/hello_server.js
@@ -2,23 +2,23 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/html'});
+    response.writeHead(200, {'Content-Type': 'text/html'});
 
-  // if (request.url === '/') {
-  //   response.write('<h1>Página principal</h1>');
-  // } else if (request.url === '/bemvindo') {
-  //   response.write('<h1>Bem-vindo :)</h1>');
-  // } else {
-  //   response.write('<h1>Página não encontrada :(</h1>');
-  // }
+    // if (request.url === '/') {
+    //   response.write('<h1>Página principal</h1>');
+    // } else if (request.url === '/bemvindo') {
+    //   response.write('<h1>Bem-vindo :)</h1>');
+    // } else {
+    //   response.write('<h1>Página não encontrada :(</h1>');
+    // }
 
-  response.write('<h1>Dados da query string</h1>');
-  const result = url.parse(request.url, true);
-  for (var key in result.query)	{
-      response.write(`<h2>${key}: ${result.query[key]}</h2>`);
-  }
+    response.write('<h1>Dados da query string</h1>');
+    const result = url.parse(request.url, true);
+    for (var key in result.query) {
+        response.write(`<h2>${key}: ${result.query[key]}</h2>`);
+    }
 
-  response.end();
+    response.end();
 });
 
 server.listen(3000, () => {
